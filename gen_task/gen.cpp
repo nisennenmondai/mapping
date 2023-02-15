822410262ca567a0beb29f4ec2dd934acadeb639
nisennenmondai <nisennenmondai@protonmail.com>
2023-02-15 14:53:24 +0100

    dev: started gen task

---
+++ gen_task/gen.cpp
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+#include <vector>
+
+using namespace std;
+
+struct task {
+        int c;
+        int t;
+        int d;
+        int p;
+        int r;
+        int id;
+};
+
+int main(void)
+{
+        return 0;
+}
